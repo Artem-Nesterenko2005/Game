9a0051ad9b4eecfd6bc2e8e8f9f7b76b1c42abb9
Ilya Sotnikov <ilia12st@yandex.ru>
2024-03-29 16:18:39 +0300

    add main loop

---
+++ Game/Game.cs
@@ -0,0 +1,8 @@
+public class Game
+{
+    public void OnLeft(object sender, EventArgs args)
+    => Console.WriteLine("Going left");
+    
+    public void OnRight(object sender, EventArgs args)
+    => Console.WriteLine("Going right");
+}
--- Game/Program.cs
+++ Game/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var eventLoop = new EventLoop();
+var game = new Game();
+
+eventLoop.LeftHandler += game.OnLeft;
+eventLoop.RightHandler += game.OnRight;
+
+var log = new List<string>();
+
+eventLoop.LeftHandler += (sender, eventArgs) => log.Add("left");
+eventLoop.RightHandler += (sender, eventArgs) => log.Add("right");
+
+eventLoop.Run();
