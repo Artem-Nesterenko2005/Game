aa3a3dc73ab902fdcc8d0434d2fba85c989ec817
Artem <hohol.amogus@mail.ru>
2024-03-29 19:22:19 +0300

    EventLoop class

---
+++ EventLoop.cs
@@ -0,0 +1,17 @@
+public class EventLoop {
+    public event EventHandler<EventArgs> LeftHandler = (sender, args) => { };
+    public event EventHandler<EventArgs> RightHandler = (sender, args) => { };
+    public void Run() {
+        while (true) {
+            var key = Console.ReadKey(true);
+            switch (key.Key) {
+                case ConsoleKey.LeftArrow:
+                    LeftHandler(this, EventArgs.Empty);
+                    break;
+                case ConsoleKey.RightArrow:
+                    RightHandler(this, EventArgs.Empty);
+                    break;
+            }
+        }
+    }
+}
